ffd1e5d7e378aaa986ce16ccbe390fb3e8df6080
mehmet <sayinmehmet47@gmail.com>
2024-01-30 21:44:22 +0100

    feat: internalization and swagger

---
+++ src/main/java/com/restfullweb/restfullweb/HelloWorldBean.java
@@ -0,0 +1,25 @@
+package com.restfullweb.restfullweb;
+
+public class HelloWorldBean {
+    private  String message;
+
+    public HelloWorldBean(String helloWorld) {
+          this.message = helloWorld;    
+
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message= message;
+    }
+
+    @Override
+    public String toString() {
+        return "HelloWorldBean{" +
+                "message='" + message + '\'' +
+                '}';
+    }
+}
---
+++ src/main/java/com/restfullweb/restfullweb/RestfullWebApplication.java
@@ -0,0 +1,13 @@
+package com.restfullweb.restfullweb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestfullWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestfullWebApplication.class, args);
+	}
+
+}

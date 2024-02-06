2215d03941c0e30fdb327e1a17ac7c63aa0141c3
mehmet <sayinmehmet47@gmail.com>
2024-02-06 22:08:04 +0100

    feat: h2 console , repository

--- src/main/java/com/restfullweb/restfullweb/user/User.java
+++ src/main/java/com/restfullweb/restfullweb/user/User.java
@@ -1,14 +1,24 @@
 package com.restfullweb.restfullweb.user;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
 import jakarta.validation.constraints.Past;
 import jakarta.validation.constraints.Size;
 import org.springframework.context.annotation.PropertySource;
 
 import java.time.LocalDate;
 
+
+@Entity(name = "user_details")
 public class User {
 
+    protected User(){}
+
+
+    @Id
+    @GeneratedValue
     private Integer id;
 
     @Size(min=2, message = "Name should be at least 2 characters")
---
+++ src/main/resources/data.sql
@@ -0,0 +1,10 @@
+insert into user_details(id,birth_date,name)
+values(10001,current_date(),'Jack');
+
+insert into user_details(id,birth_date,name)
+values(10002,current_date(),'mehmet');
+
+
+
+insert into user_details(id,birth_date,name)
+values(10003,current_date(),'Tugba');
